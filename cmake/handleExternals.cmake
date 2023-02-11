4e841bda5743efa4a96782d9ae7b497ae1ad7ba5
Kim Huber <khuber@kcloud.com>
2023-02-11 16:12:43 +0100

    rework of handlingExternals, now only a file needs to be added if a new external is added

--- cmake/handleExternals.cmake
+++ cmake/handleExternals.cmake
@@ -1,4 +1,3 @@
-#needs to be set here or CMAKE_CURRENT_LIST_DIR will point to the current folder where the function is called
 set(currentFolder ${CMAKE_CURRENT_LIST_DIR})
 
 function(handleExternals)
@@ -10,7 +9,6 @@ function(handleExternals)
         message(ERROR "Wrong number of arguments. The form is: NAME <externalVersion> [optional]VERSION <specialVersion>")
     endif()
 
-
     # if target is already there return
     if(TARGET ${HANDLE_EXTERNALS_NAME})
         return()
@@ -18,108 +16,20 @@ function(handleExternals)
 
     message(DEBUG "Try to get external ${HANDLE_EXTERNALS_NAME}")
 
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL Taskflow)
-        include(${currentFolder}/externals/taskflow.cmake)
+    file(GLOB files "${currentFolder}/externals/*")
+    set(file_list)
+    foreach(file ${files})
+        get_filename_component(filename ${file} NAME_WE)
+        list(APPEND file_list ${filename})
+    endforeach()
+
+    list(FIND file_list ${HANDLE_EXTERNALS_NAME} index)
+    if(index GREATER -1)
+        list(GET files ${index} dependencyFile)
+        include(${dependencyFile})
         return()
+    else()
+        message(WARNING "Try to get external '${HANDLE_EXTERNALS_NAME}' which can't be found")
     endif()
 
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL opengl)
-        include(${currentFolder}/externals/opengl.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL glad)
-        include(${currentFolder}/externals/glad.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL doctest)
-        include(${currentFolder}/externals/doctest.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL refl-cpp)
-        include(${currentFolder}/externals/refl-cpp.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL nlohmann_json)
-        include(${currentFolder}/externals/nlohmann_json.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL tomlplusplus_tomlplusplus)
-        include(${currentFolder}/externals/tomlplusplus_tomlplusplus.cmake)
-        return()
-    endif()
-
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL glfw)
-        include(${currentFolder}/externals/glfw.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL google_benchmark)
-        include(${currentFolder}/externals/google_benchmark.cmake)
-        return()
-    endif()
-
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL hedley)
-        include(${currentFolder}/externals/hedley.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL freetype)
-        include(${currentFolder}/externals/freetype.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL uwebsockets)
-        include(${currentFolder}/externals/u_web_sockets.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL quill)
-        include(${currentFolder}/externals/quill.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL python)
-        include(${currentFolder}/externals/python.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL libsecret)
-        include(${currentFolder}/externals/libsecret/FindLibsecret.cmake)
-        include(${currentFolder}/externals/libsecret/libsecret.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL imGui)
-        include(${currentFolder}/externals/imgui.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL imGuiNodeEditor)
-        include(${currentFolder}/externals/imGuiNodeEditor.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL stbImage)
-        include(${currentFolder}/externals/stbImage.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL mraa)
-        include(${currentFolder}/externals/mraa.cmake)
-        return()
-    endif()
-
-    if(${HANDLE_EXTERNALS_NAME} STREQUAL uWebSockets)
-        include(${currentFolder}/externals/uWebSockets.cmake)
-        return()
-    endif()
-
-    message(FATAL_ERROR "External ${HANDLE_EXTERNALS_NAME} was given which does not exist")
 endfunction()

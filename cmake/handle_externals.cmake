b615dfe784d8ae66bd1ae8fec077c1e2d9529952
TheKiHub <khuber@kcloud.com>
2023-05-01 12:23:49 +0200

    - added quill release option - optimized handle external - updated cpm - small fixes and updates

--- cmake/handle_externals.cmake
+++ cmake/handle_externals.cmake
@@ -1,4 +1,4 @@
-set(currentFolder ${CMAKE_CURRENT_LIST_DIR})
+file(GLOB ALL_EXTERNAL_FILES "${CMAKE_CURRENT_LIST_DIR}/externals/*")
 
 function(handleExternals)
     cmake_parse_arguments(HANDLE_EXTERNALS "" "NAME;VERSION" "" ${ARGN})
@@ -16,16 +16,15 @@ function(handleExternals)
 
     message(DEBUG "Try to get external ${HANDLE_EXTERNALS_NAME}")
 
-    file(GLOB files "${currentFolder}/externals/*")
     set(file_list)
-    foreach(file ${files})
+    foreach(file ${ALL_EXTERNAL_FILES})
         get_filename_component(filename ${file} NAME_WE)
         list(APPEND file_list ${filename})
     endforeach()
 
     list(FIND file_list ${HANDLE_EXTERNALS_NAME} index)
     if(index GREATER -1)
-        list(GET files ${index} dependencyFile)
+        list(GET ALL_EXTERNAL_FILES ${index} dependencyFile)
         include(${dependencyFile})
         return()
     else()

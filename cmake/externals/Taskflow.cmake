3e835b7544a119ebcd25e33087213efa6bcb97b5
Kim Huber <kim.huber.1991@googlemail.com>
2025-05-03 12:45:11 +0200

    * update versions

--- cmake/externals/Taskflow.cmake
+++ cmake/externals/Taskflow.cmake
@@ -1,7 +1,7 @@
 # https://github.com/taskflow/taskflow
 # check if special Version is used or set the standard version
 if ("${HANDLE_EXTERNALS_VERSION}" STREQUAL "")
-    set(HANDLE_EXTERNALS_VERSION "3.9.0")
+    set(HANDLE_EXTERNALS_VERSION "3.10.0")
 endif ()
 
 CPMAddPackage(
